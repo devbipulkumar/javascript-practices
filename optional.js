c1e6256f8c1f9f127b940e19cc7beb1b3af7ba29
RAHUL GUTTER <rajsaurav0987@gmail.com>
2026-06-28 00:51:24 +0530

    optional program

---
+++ optional.js
@@ -0,0 +1,24 @@
+"use strict";
+/*//optional chaining operator  ?.
+function fun_one(arg1?:string,arg2?:string,arg3?:string,){
+    console.log(arg1,arg2,arg3);
+}
+fun_one();
+fun_one("Hello_1");
+*/
+/*
+function fun_one(arg2:String,arg1?:String):void{
+    console.log(arg2,arg1);
+}
+fun_one("Hello_1");
+fun_one("Hello_1","Hello_2");
+fun_one(undefined,undefined);
+fun_one(null,null);
+*/
+function fun_one(arg1, arg2 = "Hello_2", arg3, ...arg4) {
+    console.log(arg1, arg2, arg3, arg4);
+}
+fun_one("Hello_1");
+fun_one("Hello_1", "Hello_2", "Hello_3", "Hello_4");
+fun_one(undefined, undefined, undefined, undefined);
+fun_one(null, null, null, null);
